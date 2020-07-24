d61b84ee81b1800cfea629c21bd6d1f3b4fb1899
trucktar <57882685+trucktar@users.noreply.github.com>
2020-07-24 14:30:53 +0300

    Substitute the default User model

---
+++ quiz_app/api/models/user.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email and password.
+        """
+        if username is None:
+            raise ValueError('The given username must be set')
+        if email is None:
+            raise ValueError('The given email must be set')
+        if password is None:
+            raise ValueError('The given password must be set')
+
+        email = email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        """Returns a string representation of `User` instance."""
+        return self.email
