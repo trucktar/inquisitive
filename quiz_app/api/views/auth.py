157c6c561808b179005cd50861a46b6b6a4ac9fa
trucktar <57882685+trucktar@users.noreply.github.com>
2020-07-29 14:05:07 +0300

    Refactor conditionals to be more semantic

--- quiz_app/api/views/auth.py
+++ quiz_app/api/views/auth.py
@@ -7,44 +7,46 @@ from ..serializers import LoginSerializer, SignupSerializer
 
 
 class SignupAPIView(APIView):
-    permission_classes = (AllowAny, )
+    permission_classes = [AllowAny]
     serializer_class = SignupSerializer
     http_method_names = ['post']
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                {
-                    'message': 'Registration failed!',
-                    'errors': {
-                        **serializer.errors,
-                    },
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        serializer.save()
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(
+            {
+                'message': 'Registration failed!',
+                'errors': {
+                    **serializer.errors,
+                },
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class LoginAPIView(APIView):
-    permission_classes = (AllowAny, )
+    permission_classes = [AllowAny]
     serializer_class = LoginSerializer
     http_method_names = ['post']
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        if not serializer.is_valid():
-            # Authenticating with invalid credentials returns 401 Unauthorized
-            return Response(
-                {
-                    'message': 'Bad credentials',
-                    'errors': {
-                        **serializer.errors,
-                    },
-                },
-                status=status.HTTP_401_UNAUTHORIZED,
-            )
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Authenticating with invalid credentials returns 401 Unauthorized
+        return Response(
+            {
+                'message': 'Bad credentials',
+                'errors': {
+                    **serializer.errors,
+                },
+            },
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
