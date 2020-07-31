141281e0cd73c05b43ef4ec202db3167578738b3
trucktar <57882685+trucktar@users.noreply.github.com>
2020-07-31 18:42:45 +0300

    Add refs to API views and hook up the URL mapper

--- quiz_app/api/urls.py
+++ quiz_app/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import LoginAPIView, SignupAPIView
+from .views import (AttemptView, LoginAPIView, ProfileView, QuizListView,
+                    SignupAPIView)
 
 urlpatterns = [
     path('auth/signup/', SignupAPIView.as_view()),
     path('auth/login/', LoginAPIView.as_view()),
+    path('profile/', ProfileView.as_view()),
+    path('quizzes/', QuizListView.as_view()),
+    path('attempt/', AttemptView.as_view())
 ]
--- quiz_app/api/views/__init__.py
+++ quiz_app/api/views/__init__.py
@@ -1,1 +1,3 @@
 from .auth import LoginAPIView, SignupAPIView
+from .profile import ProfileView
+from .quiz import AttemptView, QuizListView
